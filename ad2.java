683fb9138c572c9b797c09f46352679f0a2d81eb
Lucas <55569386+Blacklinkker@users.noreply.github.com>
2020-07-15 21:31:17 -0300

    adicionados os relatórios

--- ad2.java
+++ ad2.java
@@ -9,7 +9,7 @@ public class ad2 {
     public static void main(String[] args) {
         int op = 0;
         //Menu principal
-        while (op != 4) {
+        while (op != 5) {
             switch (op) {
                 case 0:
                     op = Integer.parseInt(JOptionPane.showInputDialog("Bem vindo, qual operação desejada? \n"
@@ -32,7 +32,20 @@ public class ad2 {
                 break;
 
                 case 3:
-                    op=4;
+                    int op2=Integer.parseInt(JOptionPane.showInputDialog("informe estatística desejada: \n"
+                    +"1 - Renda \n"
+                    +"2 - Etnia \n"
+                    +"3 - Sexo \n"
+                    +"4 - Escolaridade \n"
+                    +"5 - Faixa etária \n"
+                    +"6 - Orientação \n"
+                    +"7 - Emprego \n"
+                    +"8 - Deficiência \n"));
+                    exibeEstatistica(op2);
+                break;
+
+                case 4:
+                    op=5;
                 break;
 
                 default:
@@ -49,9 +62,13 @@ public class ad2 {
         for (int i = 0; i < n; i++) {
             boolean possuiDeficiencia = false;
             boolean possuiCpf = true;
+            
             String[] options = { "Sim", "Não" };
             int x;
 
+            JOptionPane.showConfirmDialog(null, "Ao aceitar, seus dados serão armazenados e utilizados"
+            +" para estatisticas anônimas, estarão disponiveis para organizações que desejem prestar auxilio");
+            
             String nome = JOptionPane.showInputDialog("Nome do entrevistado: ");
 
             x = JOptionPane.showOptionDialog(null, "Possui cpf:?", "ATENÇÃO", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
@@ -64,26 +81,26 @@ public class ad2 {
                 possuiCpf = false;
             }
 
+            String[] rendaValues = { "Não possui", "Até um salário minimo", "1000-1500", "1500-2000", "Mais de 2000" };
+            Object rendaSelected = JOptionPane.showInputDialog(null, "Renda estimada:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, rendaValues, "0");
+
             String[] etniaValues = { "Branco", "Indígena", "Negro", "Pardo", "Asiático" };
-            Object etniaSelected = JOptionPane.showInputDialog(null, "Etnia:", "Selection", JOptionPane.DEFAULT_OPTION, null, etniaValues, "0");
-
-            String[] idadeValues = { "Até 18 anos", "18-25", "26-36", "37-49", "50-60", "61 ou mais"};
-            Object idadeSelected = JOptionPane.showInputDialog(null, "Faixa etária:", "Selection", JOptionPane.DEFAULT_OPTION, null, idadeValues, "0");
-
-            String[] escolaridadeValues = { "Não possui escolaridade", "Fundamental incompleto", "Medio incompleto", "Superior incompleto", "Superior completo" };
-            Object escolaridadeSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selection", JOptionPane.DEFAULT_OPTION, null, escolaridadeValues, "0");
-
+            Object etniaSelected = JOptionPane.showInputDialog(null, "Etnia:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, etniaValues, "0");
+            
             String[] sexoValues = { "Feminino", "Masculino" };
-            Object sexoSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selection", JOptionPane.DEFAULT_OPTION, null, sexoValues, "0");
+            Object sexoSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, sexoValues, "0");
+            
+            String[] escolaridadeValues = { "Não possui escolaridade", "Fundamental incompleto", "Medio incompleto", "Superior incompleto", "Superior completo" };
+            Object escolaridadeSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, escolaridadeValues, "0");
+            
+            String[] idadeValues = { "Até 18 anos", "18-25", "26-36", "37-49", "50-60", "61 ou mais"};
+            Object idadeSelected = JOptionPane.showInputDialog(null, "Faixa etária:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, idadeValues, "0");
 
             String[] orientacaoValues = { "Heterossexual", "Homossexual", "Bissexual", "Prefiro não informar" };
-            Object orientacaoSelected = JOptionPane.showInputDialog(null, "Orientação sexual:", "Selection", JOptionPane.DEFAULT_OPTION, null, orientacaoValues, "0");
-
-            String[] rendaValues = { "Não possui", "Até um salário minimo", "1000-1500", "1500-2000", "Mais de 2000" };
-            Object rendaSelected = JOptionPane.showInputDialog(null, "Renda estimada:", "Selection", JOptionPane.DEFAULT_OPTION, null, rendaValues, "0");
+            Object orientacaoSelected = JOptionPane.showInputDialog(null, "Orientação sexual:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, orientacaoValues, "0");
 
             String[] empregoValues = { "Não possui", "Emprego fixo", "Não-CLT"};
-            Object empregoSelected = JOptionPane.showInputDialog(null, "Situação laboral: ", "Selection", JOptionPane.DEFAULT_OPTION, null, empregoValues, "0");
+            Object empregoSelected = JOptionPane.showInputDialog(null, "Situação laboral: ", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, empregoValues, "0");
 
             x = JOptionPane.showOptionDialog(null, "Possui deficiência?:", "ATENÇÃO", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
             Object deficienciaSelected = null;
@@ -91,7 +108,7 @@ public class ad2 {
             if (x == 0) {
                 possuiDeficiencia = true;
                 String[] deficiencia = { "Física", "Mental", "Visual", "Auditiva", "Multipla" };
-                deficienciaSelected = JOptionPane.showInputDialog(null, "Qual?", "Selection", JOptionPane.DEFAULT_OPTION, null, deficiencia, "0");
+                deficienciaSelected = JOptionPane.showInputDialog(null, "Qual?", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, deficiencia, "0");
             }
 
             if (possuiCpf && possuiDeficiencia) {
@@ -119,4 +136,65 @@ public class ad2 {
 
         JOptionPane.showMessageDialog(null, toString, "Registrados", JOptionPane.INFORMATION_MESSAGE);
     }
+
+    public static void exibeEstatistica(int op2){
+    
+        switch(op2){
+            case 1:
+                String[] rendaValues = { "Não possui", "Até um salário minimo", "1000-1500", "1500-2000", "Mais de 2000" };
+                Object rendaSelected = JOptionPane.showInputDialog(null, "Renda estimada:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, rendaValues, "0");
+                
+                JOptionPane.showMessageDialog(null, Relatorio.rendaCount(rendaSelected.toString(), listaDados));
+            break;
+
+            case 2:
+                String[] etniaValues = { "Branco", "Indígena", "Negro", "Pardo", "Asiático" };
+                Object etniaSelected = JOptionPane.showInputDialog(null, "Etnia:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, etniaValues, "0");
+
+                JOptionPane.showMessageDialog(null, Relatorio.etniaCount(etniaSelected.toString(), listaDados));               
+            break;
+
+            case 3:
+                String[] sexoValues = { "Feminino", "Masculino" };
+                Object sexoSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, sexoValues, "0");
+
+                JOptionPane.showMessageDialog(null, Relatorio.sexoCount(sexoSelected.toString(), listaDados));
+            break;
+
+            case 4:
+                String[] escolaridadeValues = { "Não possui escolaridade", "Fundamental incompleto", "Medio incompleto", "Superior incompleto", "Superior completo" };
+                Object escolaridadeSelected = JOptionPane.showInputDialog(null, "Grau de escolaridade:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, escolaridadeValues, "0");
+
+                JOptionPane.showMessageDialog(null, Relatorio.escolaridadeCount(escolaridadeSelected.toString(), listaDados));
+            break;
+
+            case 5:
+                String[] idadeValues = { "Até 18 anos", "18-25", "26-36", "37-49", "50-60", "61 ou mais"};
+                Object idadeSelected = JOptionPane.showInputDialog(null, "Faixa etária:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, idadeValues, "0");
+
+                JOptionPane.showMessageDialog(null, Relatorio.idadeCount(idadeSelected.toString(), listaDados));
+            break;
+
+            case 6:
+                String[] orientacaoValues = { "Heterossexual", "Homossexual", "Bissexual", "Prefiro não informar" };
+                Object orientacaoSelected = JOptionPane.showInputDialog(null, "Orientação sexual:", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, orientacaoValues, "0");
+                
+                JOptionPane.showMessageDialog(null, Relatorio.orientacaoCount(orientacaoSelected.toString(), listaDados));
+            break;
+
+            case 7:    
+                String[] empregoValues = { "Não possui", "Emprego fixo", "Não-CLT"};
+                Object empregoSelected = JOptionPane.showInputDialog(null, "Situação laboral: ", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, empregoValues, "0");
+
+                JOptionPane.showMessageDialog(null, Relatorio.empregoCount(empregoSelected.toString(), listaDados));
+            break;
+
+            case 8:
+                String[] deficiencia = { "Física", "Mental", "Visual", "Auditiva", "Multipla" };
+                Object deficienciaSelected = JOptionPane.showInputDialog(null, "Qual?", "Selecione uma opção", JOptionPane.DEFAULT_OPTION, null, deficiencia, "0");
+                
+                JOptionPane.showMessageDialog(null, Relatorio.deficienciaCount(deficienciaSelected.toString(), listaDados));
+            break;   
+        }
+    }   
 }
